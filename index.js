b0264ffb86e372ae84293ea0ee787fcf2c06c192
hoeppner.dataport <marcus.hoeppner@dataport.de>
2025-03-11 08:52:44 +0100

    output-json-as-file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import core from "@actions/core";
 import loadSboms from "./src/loadSboms.js";
 import MergedSbom from "./src/MergedSbom.js";
+import fs from "fs";
 
 const filename = core.getInput("filename") ? core.getInput("filename") : "dependencies.sbom.json";
-const reposString = core.getInput("repos");
+const outputFilename = core.getInput("filename") ? core.getInput("output-filename") : "summarized-licenses.json";
+// const reposString = core.getInput("repos");
+const reposString =
+    "hpi-schul-cloud/schulcloud-server:33.3.0;hpi-schul-cloud/schulcloud-client:33.3.0;hpi-schul-cloud/nuxt-client:33.3.0;hpi-schul-cloud/superhero-dashboard:33.3.0;hpi-schul-cloud/schulcloud-calendar:33.3.0;hpi-schul-cloud/antivirus_check_service:33.3.0;hpi-schul-cloud/version-aggregator:33.3.0;hpi-schul-cloud/h5p-staticfiles-server:33.3.0;hpi-schul-cloud/tldraw-client:33.3.0;hpi-schul-cloud/tldraw-server:33.3.0";
 
 export const run = async () => {
     try {
@@ -18,7 +22,12 @@ export const run = async () => {
         if (mergedSbom.isEmpty()) {
             throw new Error("Merged SBOM is empty");
         }
-        core.setOutput("json", mergedSbom.toString());
+
+        core.info("Writing merged SBOM to file: " + outputFilename);
+        fs.writeFileSync(outputFilename, mergedSbom.toString());
+        core.setOutput("output-filename", outputFilename);
+
+        core.info("=== 3. Done ===");
     } catch (error) {
         core.error(error);
         process.exit(1);
